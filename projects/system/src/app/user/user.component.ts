e28e121bcb53210a7eddb2273a37a56ae7535841
Ana Lilia Márquez Córdova <anamarquez2091@gmail.com>
2022-11-30 17:53:21 -0600

    cambios, inicios en componentes

--- projects/system/src/app/user/user.component.ts
+++ projects/system/src/app/user/user.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
   @Input() name: string = 'Sergio';
+  @Input() age!: number 
 
   constructor() {}
 
